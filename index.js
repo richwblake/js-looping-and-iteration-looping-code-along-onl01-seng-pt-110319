272e902ede44deebc9dd336c45e921b1efeebce5
Wills Blake <richwblake@gmail.com>
2020-06-01 20:48:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let thankYouArray = []
+    for (let index = 0; index < names.length; index++) {
+        thankYouArray.push(`Thank you, ${names[index]}, for the wonderful surprise gift!`)
+    }
+    return thankYouArray
+}
+
+function countDown(number) {
+    for (let index = 0; index <= number; index++) {
+        console.log(number - index)
+    }
+}
